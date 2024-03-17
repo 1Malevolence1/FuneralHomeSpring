2ed466fe2fbad7ab132b1f859828d0bfff7a153d
Mr.D <danil36rusdanil@mail.ru>
2024-03-17 18:49:31 +0300

    Закомитил недостающие файлы

---
+++ src/main/java/com/example/FunneralHomeNew/FuneralHomeNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.FunneralHomeNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneralHomeNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuneralHomeNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FunneralHomeNew/FunneralHomeNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FunneralHomeNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunneralHomeNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
