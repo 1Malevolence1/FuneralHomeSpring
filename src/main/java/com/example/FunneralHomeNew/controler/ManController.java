591753dd8d18180700c965d0bf38c6a27e8474d4
Mr.D <danil36rusdanil@mail.ru>
2024-04-05 13:27:45 +0300

    Простой уровень защиты

--- src/main/java/com/example/FunneralHomeNew/controler/ManController.java
+++ src/main/java/com/example/FunneralHomeNew/controler/ManController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ManController {
 
 
-    @GetMapping
+    @GetMapping("/")
     public String mainPage(){
         return "main-page";
     }
--- src/main/java/com/example/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,17 +18,33 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-
+    // авторизация
+    //authorizeHttpRequests - используется для настроки http авторизации
+    // requestMatchers - указываем параметры запросы, к которым будут применяться те или иные правила доступа
+    // denyAll() - никто не моежт получить доступ
+    //anonymous() - досут может иметь только не аунтифицированный пользователь
+    //authenticated() - пользоватлеь для указанной страницы должен быть анутифицирован
+    // remeberMe() - предоставляет доступ пльзователям с долго живущей ссией
+    // fullyAuthenticated() - предосталвяем доступ, когда пользователь прошёл полную авторизацию
+    // hasAuthority("право") - предоставляет доступ с определённым правом
+    //hasAnyAuthority() -   пользователь имеет хотябы одно из право
+    // hasRole() - если у пользователя есть роль
+    // hasAnyRole - хотябы одна роль
+    // access() -> предоставляет функциональный подход к определению правил доступая
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((requests) -> requests.requestMatchers("/login").permitAll().requestMatchers("/**").authenticated())
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll).build();
+        return http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/main-page/**").permitAll().anyRequest().authenticated())
+                .formLogin(fromLogin -> fromLogin.loginPage("/login").permitAll().defaultSuccessUrl("/", true)).
+                logout(LogoutConfigurer::permitAll).build();
 
     }
 
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+
         UserDetails admin = User.withUsername("admin").password(passwordEncoder.encode("123")).roles("ADMIN").build();
         UserDetails user = User.withUsername("user").password(passwordEncoder.encode("123")).roles("USER").build();
 
